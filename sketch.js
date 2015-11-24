0baa54877c031c60cd85021e6065aaf4785bf62d
jckhang <yz3079@nyu.edu>
2015-11-24 15:56:03 -0500

    First pages commit

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var input;
+
+function setup() {
+  noCanvas();
+
+  input = select('#search');
+  var button = select('#submit');
+  button.mousePressed(search);
+}
+
+function search() {
+  var term = input.value();
+  var url = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&format=json' + '&titles=' + term;
+  loadJSON(url, gotData, 'jsonp');
+}
+
+function gotData(data) {
+  for (key in data.query.pages) {
+
+    var birthdeath = (data.query.pages[key].revisions[0]["*"]);
+
+    if (birthdeath.indexOf("|df=yes|") > 0) {
+      var deathdateA = (birthdeath.split("{{Death date and age|df=yes|")[1].split("}}")[0].split("|"));
+      createP(input.value() + ":" + deathdateA[3] + "-" + deathdateA[0]);
+
+    }
+    if ((birthdeath.indexOf("df=y}}") > 0) || (birthdeath.indexOf("mf=y") > 0)) {
+      var deathdateB = (birthdeath.split("{{death date and age|")[1].split("}}")[0].split("|"));
+      createP(input.value() + ":" + deathdateB[3] + "-" + deathdateB[0]);
+
+    }
+
+  }
+}
+
+// if (keywordA.indexOf("|df=yes|") > 0) {
+//   var deathdateA = (birthdeath.split("{{Death date and age|df=yes|")[1].split("}}")[0].split("|"));
+//   createP(input.value() + ":" + deathdateA[3] + "-" + deathdateA[0]);
+
+// }
+
+// function draw(){
+//   fill(0);
+//   rect(10,10,50,50);
+
+// }
+
+//   for (key in data.query.pages) {
+//     var birthdeath = (data.query.pages[key].revisions[0]["*"].split("Death date and age|df=yes|")[1].split("}}")[0].split("|"));
+//     println(input.value() + ":" + birthdeath[3] + "-" + birthdeath[0]);
+//     createP(input.value() + ":" + birthdeath[3] + "-" + birthdeath[0]);
+
+
+//   }
+// }
+
+
+
+// println(data.query.pages[key].revisions[0]["*"].split("Death date and age|df=yes|")[1].split("}}")[0].split("|"));
+//   createP(input.value()
+//   + ":"
+//   + data.query.pages[key].revisions[0]["*"].split("Death date and age|df=yes|")[1].split("}}")[0].split("|")[3])
+//   ;
