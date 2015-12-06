621bba72e0a229486121a50448c7862f63029033
jckhang <yz3079@nyu.edu>
2015-12-05 23:25:41 -0500

    update

--- sketch.js
+++ sketch.js
@@ -6,9 +6,8 @@ var h1;
 var canvas;
 
 function setup() {
-   canvas = createCanvas(1075,500);
-   canvas.position(20,180);
-
+  canvas = createCanvas(1075,500);
+  canvas.position(20,180);
 
   background(0);
   nameP = createP('Search births by his/her name');
@@ -399,45 +398,3 @@ function gotData(data) {
     }
   }
 }
-
-
-
-// function draw() {
-// var btn = document.getElementById('submit');
-//     btn.addEventListener('click', function() {
-//       //randomSeed(5);
-//   fill(100, 200, 10);
-//       rect(10, random(height), random(width), 10);
-//   });
-//   }
-
-
-//   if (wikiData) {
-//     for (var i = 0; i < height; i++ ){
-//     fill(100, 200, 10);
-//     randomSeed(4);
-//       rect(10, random(height), random(width), 10);
-// }
-//}
-
-
-// var X_AXIS = 2;
-//       var c1;
-//       c1 = color(0);
-//       c2 = color(colorBA, 255, 0, colorBA);
-//       setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
-
-//       createP(input.value() + ":" + birthdateA + "-");
-
-//       function setGradient(x, y, w, h, c1, c2, axis) {
-
-//         noFill();
-//         if (axis == X_AXIS) {
-//           for (var i = wordLocationXBA; i <= x + w; i++) {
-//             var inter = map(i, x, x + w, 0, 1);
-//             var c = lerpColor(c1, c2, inter);
-//             stroke(c);
-//             line(i, wordLocationYBA, i, wordLocationYBA + 1);
-//           }
-//         }
-//       }
