d5e5d87db3363bb1699a78079432e38159382672
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-01-29 12:12:56 -0500

    Update passport Oauth

--- server/app.js
+++ server/app.js
@@ -4,15 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var swig = require('swig');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 
 // view engine setup
+var swig = new swig.Swig();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +24,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//===============Passport===============        
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//===============MongoDB===============
+var mongoose = require('mongoose');
+// *** mongoose *** //
+mongoose.connect('mongodb://localhost/passport-social-auth');
+
+//===============routes===============        
+var routes = require('./routes/index');
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,8 +49,8 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
+//===============Error Handlers===============
+        
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
