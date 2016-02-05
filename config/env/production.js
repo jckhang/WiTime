25befb9371be75f168064a73349964b7e1e6990d
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-02-04 19:21:13 -0500

    update mongolab

--- config/env/production.js
+++ config/env/production.js
@@ -5,7 +5,7 @@
  */
 
 module.exports = {
-  db: process.env.MONGOHQ_URL,
+  db:  process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || ,
   facebook: {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
