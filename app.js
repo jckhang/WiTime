b38a801ea1c892612ade6ac16daa404eeb66cf37
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-05-05 03:18:59 -0400

    Enable insert to database

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+'use strict';
 const express = require("express"),
     path = require('path'),
-    swig = require('swig');
+    swig = require('swig'),
+    MongoClient = require('mongodb').MongoClient,
+    ObjectId = require('mongodb').ObjectID,
+    assert = require('assert'),
+    bodyParser = require('body-parser');
 
 
 const app = express();
@@ -16,6 +21,34 @@ app.set('view engine', 'html'); // what file extension do our templates have
 app.engine('html', swig.renderFile); // how to render html templates
 swig.setDefaults({ cache: false });
 
+let url = 'mongodb://user:123@ds059185.mlab.com:59185/heroku_k6td0nss';
+
+var insertDocument = function(db, name, birth, death, callback) {
+    db.collection('peoples').insertOne({
+        "name": name,
+        "birth": birth,
+        "death": death
+    }, function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted a document into the peolpes collection.");
+    });
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/peoples', function(req, res, next) {
+    console.log(req.body.name);
+    var name = req.body.name,
+        birth = req.body.birth,
+        death = req.body.death;
+    console.log(name)
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        insertDocument(db, name, birth, death, function() {
+            db.close();
+        });
+    });
+});
 routes(app);
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
